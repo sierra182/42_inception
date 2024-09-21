618ee307adcf4a22a5d5e169b20e483ee346cf0c
sierra182 <sierra-echo@msn.com>
2024-09-21 21:43:21 +0200

    wip night

--- inception/srcs/requirements/mariadb/tools/init_db.sh
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
+# echo lolo 0
+# mysqld_safe --datadir="/var/lib/mysql" &
+# echo lili 1
+# sleep 10
+# echo dodo 2
+# mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-# Démarre le serveur MariaDB
+# mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "CREATE USER IF NOT EXISTS \`$(cat ${MYSQL_USER_FILE})\`@'%' IDENTIFIED BY '$(cat ${MYSQL_PASSWORD_FILE})';"
+
+# mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`$(cat ${MYSQL_USER_FILE})\`@'%';"
+
+# mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MYSQL_ROOT_PASSWORD_FILE})';"
+
+# mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "FLUSH PRIVILEGES;"
+# echo cho 3 
+# mysqladmin -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) shutdown
+# echo lolo 4
+
+# # Relance MariaDB en mode sûr pour rester en exécution
+# exec mysqld_safe
+
+# echo lolo 5
+
+echo "lolo 042"
 mysqld_safe --datadir="/var/lib/mysql" &
+# Démarre MariaDB en mode sûr sans utiliser '&'
+# mysqld --datadir="/var/lib/mysql" --socket=/run/mysqld/mysqld.sock
 
-# Attendre que MariaDB soit prêt
-sleep 10
+# service mysqld_safe --datadir="/var/lib/mysql" start
+service mysql start
+sleep 15
+echo "dodo 2"
 
-# Crée la base de données
+# Exécute les commandes SQL une fois que MariaDB est prêt
 mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-
-# Crée l'utilisateur et lui donne les droits
 mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "CREATE USER IF NOT EXISTS \`$(cat ${MYSQL_USER_FILE})\`@'%' IDENTIFIED BY '$(cat ${MYSQL_PASSWORD_FILE})';"
-
 mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`$(cat ${MYSQL_USER_FILE})\`@'%';"
-
-# Modifie le mot de passe de l'utilisateur root
 mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MYSQL_ROOT_PASSWORD_FILE})';"
-
-# Applique les changements
 mysql -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) -e "FLUSH PRIVILEGES;"
 
-# Arrête MariaDB proprement
+echo "cho 3"
+
+# Arrête MariaDB après avoir exécuté les commandes SQL
 mysqladmin -u root -p$(cat ${MYSQL_ROOT_PASSWORD_FILE}) shutdown
 
-# Relance MariaDB en mode sûr pour rester en exécution
-exec mysqld_safe
-
+echo "lolo 4"
+# service mysqladmin stop
+# Redémarre MariaDB en mode sûr et le laisse en exécution comme processus principal
+exec mysqld_safe # --datadir="/var/lib/mysql"
