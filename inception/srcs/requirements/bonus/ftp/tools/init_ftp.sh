54a18fa5e5f6e533f167d8a121e15ec6f9ff02bf
sierra182 <sierra-echo@msn.com>
2024-10-04 11:34:54 +0200

    wip secure ftp

--- inception/srcs/requirements/bonus/ftp/tools/init_ftp.sh
+++ inception/srcs/requirements/bonus/ftp/tools/init_ftp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-useradd -g www-data -d /var/www/wordpress -s /bin/bash $FTP_USER
-echo "$FTP_USER:$FTP_PASS" | chpasswd
-vsftpd /etc/vsftpd.conf
+
+# useradd -g www-data -d /var/www/wordpress -s /bin/bash $FTP_USER
+# echo "$FTP_USER:$FTP_PASS" | chpasswd
+
+useradd -g www-data -d /var/www/wordpress -s /bin/bash ftpuser
+echo "ftpuser:password" | chpasswd
+
+exec "$@"
